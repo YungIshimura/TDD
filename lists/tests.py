352f9755c7f67fbf8d186d3ba99587e29982a343
YungIshimura <astr6.6.6@yandex.ru>
2023-07-11 21:14:18 +0200

    First working module test

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import view_index
 
+class HomePageTest(TestCase):
+    '''Тест домашней страницы'''
 
-class SmokeTest(TestCase):
-    '''Тест на токсичность'''
-
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''Корневой url преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, view_index)
+    
